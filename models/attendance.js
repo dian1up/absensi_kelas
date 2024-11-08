6e3768de597dda9f205e917b4af5e50ba9c6f518
BayuTyz <jigen5608@gmail.com>
2024-11-08 14:08:59 +0700

    add:0

--- models/attendance.js
+++ models/attendance.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class attendance extends Model {
     /**
@@ -13,21 +11,27 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  attendance.init({
-    user_id: DataTypes.INTEGER,
-    tanggal: DataTypes.DATE,
-    clockin_date: DataTypes.DATE,
-    clockin_latitude: DataTypes.FLOAT,
-    clockin_longitude: DataTypes.FLOAT,
-    clockout_date: DataTypes.DATE,
-    clockout_latitude: DataTypes.FLOAT,
-    clockout_longitude: DataTypes.FLOAT,
-    is_leave: DataTypes.BOOLEAN,
-    leave_note: DataTypes.STRING,
-    attachment: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'attendance',
-  });
+  attendance.init(
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: { model: "users", key: "id" },
+      },
+      tanggal: DataTypes.DATE,
+      clockin_date: DataTypes.DATE,
+      clockin_latitude: DataTypes.FLOAT,
+      clockin_longitude: DataTypes.FLOAT,
+      clockout_date: DataTypes.DATE,
+      clockout_latitude: DataTypes.FLOAT,
+      clockout_longitude: DataTypes.FLOAT,
+      is_leave: DataTypes.BOOLEAN,
+      leave_note: DataTypes.STRING,
+      attachment: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "attendance",
+    }
+  );
   return attendance;
 };
